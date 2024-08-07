53f0f85c31ce64e3d0a0acf006f66dd50d741ba0
GaretKean <167364720+GaretKean@users.noreply.github.com>
2024-08-07 21:50:33 +0000

    just a bit more styling

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
+
+
+
 const Home = () => {
+	const [ inputValue, setInputValue ] = useState("");
+	const [ todos, setTodos ] = useState([]);
+
+	const handleAddTodo = (e) => {
+		if (e.key === "Enter" && inputValue.trim() !== "") {
+			setTodos([...todos, inputValue.trim()]);
+			setInputValue("");
+		}
+	};
+	const handleDeleteTodo = (index) => {
+		setTodos(todos.filter((todo, i) => index !== i))
+	};
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div className="container">
+			<h1 className="text-center mt-5">todos</h1>
+			<div className="card todo-card mx-auto mt-5" style={{ maxWidth : "800px" }} >
+				<ul className="list-group list-group-flush">
+					<li className="list-group-item">
+						<input
+							type="text"
+							onChange={(e) => setInputValue(e.target.value)}
+							value={inputValue}
+							onKeyDown={handleAddTodo}
+							placeholder="Add to your tasks"
+						/>
+					</li>
+					{todos.length === 0 ? (
+						<li className="list-group-item no-tasks">no tasks, add a task</li>
+					) : (
+						todos.map((todo, index) => (
+							<li className="list-group-item" key={index}>
+								<div className="list-group-item-todo" id="screen">
+									{todo}
+								</div>
+								<span className="x-container" onClick={() => handleDeleteTodo(index)}>
+									<i className="fa-solid fa-x"></i>
+								</span>
+							</li>
+						))
+					)}
+				</ul>
+				<div className="card-footer text-secondary">
+					{todos.length} {todos.length === 1 ? "item" : "items"} left.
+				</div>
+			</div>
+		</div>	
 	);
 };
 
